174a861a38525b6382f7793cf4e5591f7c6879f9
tyhclint <CTAN284@e.ntu.edu.sg>
2026-05-31 01:06:13 +0800

    chore/ added frontend url into CORS

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://worldcuppicks.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
