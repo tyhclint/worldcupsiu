4f43081a72ac3ebc88cca859c5458912f46c330a
tyhclint <CTAN284@e.ntu.edu.sg>
2026-05-31 16:47:31 +0800

    feat/ initial fantasy squad skeleton

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useSyncExternalStore } from 'react';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 import { Trophy, Users, LogIn, LogOut, Menu, X, Info } from 'lucide-react';
